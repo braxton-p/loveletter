8268c38b7bcd07b7a949aa167792219ab8f2ef16
bx <50059391+braxton-p@users.noreply.github.com>
2022-07-21 20:03:47 -0400

    add basic FormEvent

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,19 @@ import type { Component } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+
+class FormEvent {
+  id: number;
+  data: object;
+  dateCreated: number;
+
+  constructor(data: object) {
+    this.id = 1;
+    this.data = data;
+    this.dateCreated = Date.now();
+  }
+}
+
 const App: Component = () => {
   return (
     <div class={styles.App}>
@@ -11,14 +24,7 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        In pain
       </header>
     </div>
   );
